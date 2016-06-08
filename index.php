c921a47eeb37b59d8e22929fb37ea5575ee313cd
mint <michaelazona@gmail.com>
2016-06-08 14:17:35 -0500

    Update

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+	echo "<html><head><title>Test Page</title></head></html>";
+
 	echo "This is a test.";
 	echo "<br>This will only be published if there aren't any syntax errors.";
 	echo "<p style='color: green'>And this text is green.</p>";
