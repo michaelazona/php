de0868aa2fc236936d98df486177ee72ffdd0b77
mint <michaelazona@gmail.com>
2016-06-13 15:14:33 -0500

    This is the final version for a while - complete with PHPUnit testing, reports, and post-check builds.

--- Misc/variousPHPUnitTests.php
+++ Misc/variousPHPUnitTests.php
@@ -4,11 +4,21 @@
 
 	class PersonTest extends PHPUNIT_Framework_TestCase
 	{
-		//incase there were any changes to database connection function that caused issues with the web page's ability to connect to the database
+		//incase there were any alterations to the SQL statment - this will make sure the query is always the right one!
 		public function testSqlStatement()
 		{
 			$sql = 'INSERT INTO SampleTable ' . '(firstName, lastName) ' . 'VALUES ( "Donald", "Duck" )';
 			$this->assertEquals(assembleSqlStatment("Donald", "Duck"), $sql);
+		}
+
+
+		//checks to make sure database can still be connected to - incase someone messed with the credentials, dbname, etc.
+		public function testDatabaseConnectionFunction()
+		{
+			$conn = connectToDatabase();
+
+			if($conn)  { $this->assertTrue(TRUE) ;}
+			else       { $this->assertTrue(FALSE);}
 		}
 	}
 ?>
